b09d6952338d71889aad138361998ab715e34d8d
winstonrwhite <winston.white@my.liu.edu>
2019-10-04 14:27:36 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,4 +1,25 @@
-var cards=["queen", "queen", "king", "king"];
+var cards=[
+{
+	rank: "queen",
+	suit: "hearts",
+	cardImage: "images/queen-of-hearts.png"
+},
+{
+	rank: "queen",
+	suit: "diamonds",
+	cardImage: "images/queen-of-diamonds.png"
+},
+{
+	rank: "king",
+	suit: "hearts",
+	cardImage: "images/king-of-hearts.png"
+},
+{
+	rank: "king",
+	suit: "diamonds",
+	cardImage: "images/king-of-diamonds.png"
+}
+];
 
 var cardsInPlay=[];
 
@@ -8,13 +29,17 @@ function checkForMath(){
 } else {
 	alert("Sorry, try again");
 }
+};
+
 function flipCard(cardID){
 flipCard(0);
 flipCard(2);
 checkForMath()
 
-cardsInPlay.push(cards[cardId])
-console.log("User flipped " + cards[0]);
+cardsInPlay.push(cards[cardId].rank);
+console.log("User flipped " + cards[cardId].rank);
+console.log(cardImage);
+console.log(suit);
 }
-}
+
 
