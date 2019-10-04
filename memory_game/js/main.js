cce9bfd1e55fc1ea8ff5d77738076257d39dc975
winstonrwhite <winston.white@my.liu.edu>
2019-10-04 13:02:52 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,61 +1,23 @@
 
-var cards=[
-{
-	rank: 'queen',
-	suit: 'hearts',
-	cardImage: "images/queen-of-hearts.png"
-},
-{
-   rank: 'queen',
-	suit: 'diamonds',
-	cardImage: "images/queen-of-diamonds.png",
-},
-{
-	rank: 'king',
-	suit: 'hearts',
-	cardImage: "images/king-of-hearts.png",
-},
-{
-	rank: 'king',
-	suit: 'diamonds',
-	cardImage: "images/king-of-diamonds.png",
-},
-];
+var cards=["queen", "queen", "king", "king"];
 
 var cardsInPlay=[];
 
-function checkForMath(){
+var cardOne = ["queen"];
 
-	if(cardsInPlay.length===2){
+cardsInPlay.push('cardOne[0]');
 
-    console.log("You found a match!");
-  
-   } else if (cardsInPlay[0]===cardsInPlay[2]){
-    
-     console.log("Sorry, try again.");
-  }
+console.log("User flipped" + " " + cardOne);
 
+var cardTwo = ["king"];
+
+cardsInPlay.push('cardTwo[2]');
+
+console.log("User flipped" + " " + cardTwo);
+
+if(cardsInPlay.length===2){
+	alert("You found a match");
+} else if(cardsInPlay[0]===cardsInPlay[2]){
+	alert("Sorry, try again");
 }
-
-checkForMath()
-
-
-function flipCard(cardID){
-
-console.log("cards[cardID].rank");
-console.log("cardImage");
-console.log("suit");
-
-flipcard(0);
-flipcard(2);
-
-cardsInPlay.push(cards[cardID].rank);
-
-}
-
-
-
-
-
-
 
