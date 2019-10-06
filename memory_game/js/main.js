970b6a3e867232e8f1c9760af59f4839858fe7d0
winstonrwhite <winston.white@my.liu.edu>
2019-10-06 00:11:11 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,18 +1,17 @@
-var cards = ["queen", "queen", "king", "king"];
+var cards=["queen", "queen", "king", "king"];
 
-var cardsInplay = [];
+var cardsInPlay = [];
 
 function checkForMath(){
-if(cardsInplay[0]===cardsInplay[1]){
-   alert("You found a match!")
+if(cardsInPlay[0]===cardsInPlay[1]){
+   console.log("You found a match!");
 } else{
-	alert("Sorry, try again");
+	console.log("Sorry, try again");
 }
 }
-
-function flipCard(carId){
-cardsInplay.push(cards[carId])
-console.log("User flipped " + cards[0]);
+function flipCard(cardId){
+cardsInPlay.push(cards[cardId]);
+console.log("User flipped " + cards[cardId]);
 checkForMath()
 
 }
