33e8950957bb1506ba854f9f0d0dba2dff12081d
winstonrwhite <winston.white@my.liu.edu>
2019-10-05 20:04:46 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,45 +1,7 @@
-var cards=[
-{
-	rank: "queen",
-	suit: "hearts",
-	cardImage: "images/queen-of-hearts.png"
-},
-{
-	rank: "queen",
-	suit: "diamonds",
-	cardImage: "images/queen-of-diamonds.png"
-},
-{
-	rank: "king",
-	suit: "hearts",
-	cardImage: "images/king-of-hearts.png"
-},
-{
-	rank: "king",
-	suit: "diamonds",
-	cardImage: "images/king-of-diamonds.png"
-}
-];
+var cardOne = "queen";
+var cardTwo = "queen";
+var cardThree = "king";
+var cardFour = "king";
 
-var cardsInPlay=[];
-
-function checkForMath(){
- if(cardsInPlay[0]===cardsInPlay[1]){
-	alert("You found a match");
-} else {
-	alert("Sorry, try again");
-}
-};
-
-function flipCard(cardID){
-flipCard(0);
-flipCard(2);
-checkForMath()
-
-cardsInPlay.push(cards[cardId].rank);
-console.log("User flipped " + cards[cardId].rank);
-console.log(cardImage);
-console.log(suit);
-}
-
-
+console.log("User flipped " + cardOne);
+console.log("User flipped " + cardTwo);
